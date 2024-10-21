a94b054463b6bace3fdc2f079c6f2a0541c1edec
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2024-10-21 07:11:49 -0300

    a

--- utils.c
+++ utils.c
@@ -28,13 +28,26 @@ struct keysPredict* keysPredictNew() {
 }
 
 void keysPredictAddWord(struct keysPredict* kt, char* word) {
+    // tengo que hacer un algoritmo que pase por todas las 
+    struct node* current = kt->first;
+    if(current == 0) { // Caso vacío
+        current = (struct node*) malloc(sizeof(struct node));
+        current->character = word[0];
+        current->end = 0;
+        current->word = 0; // no puedo asegurar que la primera letra no es una palabra
+        current->next = 0;
+        current->down = 0;
+        kt->first = current;
+        kt->totalKeys++;
+    }
 
-    // COMPLETAR
+    
 
 }
 
 void keysPredictRemoveWord(struct keysPredict* kt, char* word) {
-
+    kt->totalWords--; // quito una palabra de la cantidad total
+    
     // COMPLETAR
 }
 
@@ -95,9 +108,9 @@ void keysPredictPrintAux(struct node* n, int level) {
 
 int findPosInAlphabet(char character){ // Esta función nos permite encontrar la posición del caracter en el abecedario
     int pos = 0;
-    char* alphabet = "abcdefghijklmnopqrstuvwyz";
+    char* alphabet = "abcdefghijklmnopqrstuvwxyz";
     for(int i = 0; i<26; i++){
-        if(i == character){
+        if(alphabet[i] == character){
             return i;
         }
     }
@@ -106,7 +119,7 @@ int findPosInAlphabet(char character){ // Esta función nos permite encontrar la
 
 struct node* findNodeInLevel(struct node** list, char character) {
     struct node* lista = *list; // Hago una reasignación de la lista (desreferenciamos el doble puntero ya que está de más)
-    while(lista != NULL){ // Recorre cada nodo en la lista hasta llegar a NULL
+    while(lista != 0){ // Recorre cada nodo en la lista hasta llegar a 0
         if(lista->character == character){ // Si el caracter del nodo actual coincide con el buscado...
             return lista; // ...retornamos el nodo en el cual se encuentra la coincidencia
         }
@@ -116,27 +129,28 @@ struct node* findNodeInLevel(struct node** list, char character) {
 }
 
 struct node* addSortedNewNodeInLevel(struct node** list, char character) {
-    struct node* newNode = 0;
+    struct node* newNode = (struct node*) malloc(sizeof(struct node*));
     newNode->character = character;
+    newNode->next = 0;
 
     struct node* lista = *list; // reasignación de list
 
     int charPos = findPosInAlphabet(character);
 
     if(lista == 0){ // posible caso en el cual la lista esté vacía
-        newNode->next = 0;
+       *list = newNode;
         return newNode;
     } 
 
     if(charPos < findPosInAlphabet(lista->character)) { // Posible caso en el cual character es el más chico
         newNode->next = lista;
         *list = newNode;
-        return newNode;
+        return *list;
     }
     
-    while(lista->next != 0){
+    while(lista->next != 0){ 
         if(findPosInAlphabet(lista->character) < charPos && findPosInAlphabet(lista->next->character) > charPos){ // Si el caracter esta más adelante en el abecedario...
-                newNode->next = lista->next; // el siguiente nodo será el next de newNode
+                newNode->next = lista->next; // el siguiente de newNode va a ser el nodo actual
                 lista->next = newNode; // el siguiente nodo del acutal será newNode
                 return *list; // Cortamos ahí, retornamos el principio 
         } 
